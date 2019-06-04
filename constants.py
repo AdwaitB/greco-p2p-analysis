66ce2de6a24a7af45bbc692cef6cb3d63ee9ac57
BAUSKAR Adwait Jitendra <adwait-jitendra.bauskar@inria.fr>
2019-06-04 07:36:22 +0200

    Add the constants

---
+++ constants.py
@@ -0,0 +1,11 @@
+scale = 128
+
+data_folders= {
+    1: 'data/3d_09-05-2019',
+    2: 'data/1w_03-05-2019',
+    3: 'data/1w_10-05-2019',
+    4: 'data/1w_17-05-2019',
+    5: 'data/2w_01-05-2019'
+}
+
+data = data_folders[1]
